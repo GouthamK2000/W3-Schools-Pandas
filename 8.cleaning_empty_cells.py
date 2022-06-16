9e196d403f2807cb91e013054ee1997d64624d1a
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-06-16 22:38:02 +0530

    way of cleaning empty cells in a data set

---
+++ 8.cleaning_empty_cells.py
@@ -0,0 +1,30 @@
+#Case-1- Simply removing the empty cells
+
+import pandas as pd
+
+df=pd.read_csv('data.csv')
+new_df=df.dropna()
+print(new_df.to_string())       
+
+#dropna() returns a new datafram and wont modify the original.
+
+
+#case-2- If we want to change the original dataframe
+
+import pandas as pd
+
+df=pd.read_csv('data.csv')
+df.dropna(inplace=True)
+print(df.to_string())
+
+#Note: Now, the dropna(inplace = True) will NOT return a new DataFrame, but it will remove all rows containg NULL values from the original DataFrame.
+
+
+#Case-3-Replace empty cells with some values
+
+import pandas as pd
+
+df=pd.read_csv('data.csv')
+df.fillna(130,inplace=True)  #Value we want to substitute in empty cells
+
+print(df.to_string())
